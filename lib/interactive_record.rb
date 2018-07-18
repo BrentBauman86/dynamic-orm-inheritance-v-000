fe951553af561a588254e9ac8faba407baf00b32
Brent Bauman <brentbauman86@gmail.com>
2018-07-18 00:09:39 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -50,7 +50,7 @@ class InteractiveRecord
 
   def col_names_for_insert
     binding.pry
-    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+    self.class.column_names.delete_if {|col| col == "id"}
   end
 
   def self.find_by_name(name)
